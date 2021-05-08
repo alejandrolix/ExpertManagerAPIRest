7447fb909a5abe586fa026892084efdb28047b6c
Alejandro Pons <alejandro.pons98@gmail.com>
2021-05-08 16:34:01 +0200

    Creado clase Mensaje

---
+++ APIRest/Models/Mensaje.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest.Models
+{
+    public class Mensaje
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string Descripcion { get; set; }
+
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+    }
+}
