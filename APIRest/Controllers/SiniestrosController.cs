9ed48cf6154db6bb6f348a62b967835434314f33
Alejandro Pons <alejandro.pons98@gmail.com>
2021-05-09 09:30:21 +0200

    Creado controlador Siniestros y añadido método Index

---
+++ APIRest/Controllers/SiniestrosController.cs
@@ -0,0 +1,108 @@
+﻿using APIRest.Context;
+using APIRest.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SiniestrosController : ControllerBase
+    {
+        private ExpertManagerContext _contexto;
+
+        public SiniestrosController(ExpertManagerContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        // GET: SiniestrosController
+        [HttpGet]
+        public async Task<List<Siniestro>> Index()
+        {
+            List<Siniestro> siniestros = await _contexto.Siniestros
+                                                        .Include(siniestro => siniestro.Aseguradora)
+                                                        .Include(siniestro => siniestro.Estado)
+                                                        .Include(siniestro => siniestro.UsuarioCreado)
+                                                        .Include(siniestro => siniestro.Perito)
+                                                        .Include(siniestro => siniestro.Danios)
+                                                        .ToListAsync();
+
+            return siniestros;
+        }
+
+        // GET: SiniestrosController/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        //// GET: SiniestrosController/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: SiniestrosController/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: SiniestrosController/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: SiniestrosController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: SiniestrosController/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: SiniestrosController/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
