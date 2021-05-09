ddb0b673b95fde1be11d4ee96e4eb685102ef9ea
Alejandro Pons <alejandro.pons98@gmail.com>
2021-05-09 18:41:16 +0200

    Creado clase CrearSiniestroVm

---
+++ APIRest/ViewModels/CrearSiniestroVm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest.ViewModels
+{
+    public class CrearSiniestroVm
+    {
+        public int IdUsuarioAlta { get; set; }
+        public int IdAseguradora { get; set; }
+        public int IdEstado { get; set; }
+        public string Direccion { get; set; }
+        public string Descripcion { get; set; }
+        public int IdDanio { get; set; }
+        public int IdSujetoAfectado { get; set; }
+        public int IdPerito { get; set; }        
+    }
+}
