0764aff2119b4140d5d8853c0972ce381011b184
Alejandro Pons <alejandro.pons98@gmail.com>
2021-05-09 18:15:58 +0200

    Creado controlador Danios

---
+++ APIRest/Controllers/DaniosController.cs
@@ -0,0 +1,36 @@
+﻿using APIRest.Context;
+using APIRest.Models;
+using APIRest.ViewModels;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest.Controllers
+{    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DaniosController : ControllerBase
+    {
+        private ExpertManagerContext _contexto;
+
+        public DaniosController(ExpertManagerContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        // GET: DaniosController
+        [HttpGet]
+        public async Task<List<Danio>> Index()
+        {
+            List<Danio> danios = await _contexto.Danios                                                        
+                                                .ToListAsync();
+
+            return danios;
+        }        
+    }
+}
