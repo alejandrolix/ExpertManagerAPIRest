2bf994c790deaebfbcb1293cd1759542e7f94834
Alejandro Pons <alejandro.pons98@gmail.com>
2021-05-09 18:00:57 +0200

    Creado controlador Estados

---
+++ APIRest/Controllers/EstadosController.cs
@@ -0,0 +1,36 @@
+﻿using APIRest.Context;
+using APIRest.Models;
+using APIRest.ViewModels;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest.Controllers
+{    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstadosController : ControllerBase
+    {
+        private ExpertManagerContext _contexto;
+
+        public EstadosController(ExpertManagerContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        // GET: EstadosController
+        [HttpGet]
+        public async Task<List<Estado>> Index()
+        {
+            List<Estado> siniestros = await _contexto.Estados                                                        
+                                                     .ToListAsync();
+
+            return siniestros;
+        }        
+    }
+}
