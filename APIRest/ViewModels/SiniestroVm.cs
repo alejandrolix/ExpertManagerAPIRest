f27cc979cdd63ceabad6e0251dd0f69246b74cf5
Alejandro Pons <alejandro.pons98@gmail.com>
2021-05-09 15:48:50 +0200

    Creada clase SiniestroVm

---
+++ APIRest/ViewModels/SiniestroVm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest.ViewModels
+{
+    public class SiniestroVm
+    {
+        public int Id { get; set; }
+        public string Estado { get; set; }
+        public string Aseguradora { get; set; }
+        public string Descripcion { get; set; }
+        public string Perito { get; set; }
+        public DateTime FechaHoraAlta { get; set; }
+        public string SujetoAfectado { get; set; }
+        public decimal ImpValoracionDanios { get; set; }
+    }
+}
