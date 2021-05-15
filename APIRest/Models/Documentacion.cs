21e2c2a41daa8494475d762439dd22f98ef00072
Alejandro Pons Antelo <alejandro.pons98@gmail.com>
2021-05-15 09:44:38 +0200

    Creado clase Documentación

---
+++ APIRest/Models/Documentacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest.Models
+{
+    public class Documentacion
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public int SiniestroId { get; set; }
+        public Siniestro Siniestro { get; set; }
+    }
+}
