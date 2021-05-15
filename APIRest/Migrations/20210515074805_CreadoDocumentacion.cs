3fd88851662a3197347eb965fd4ff9bc9eed103d
Alejandro Pons Antelo <alejandro.pons98@gmail.com>
2021-05-15 09:50:07 +0200

    Creado migración

---
+++ APIRest/Migrations/20210515074805_CreadoDocumentacion.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APIRest.Migrations
+{
+    public partial class CreadoDocumentacion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Documentaciones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SiniestroId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Documentaciones", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Documentaciones_Siniestros_SiniestroId",
+                        column: x => x.SiniestroId,
+                        principalTable: "Siniestros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Documentaciones_SiniestroId",
+                table: "Documentaciones",
+                column: "SiniestroId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Documentaciones");
+        }
+    }
+}
