9bdfa355b779d993cd8ec658bcf8044f69e02979
Alejandro Pons Antelo <alejandro.pons98@gmail.com>
2021-05-15 09:52:06 +0200

    Creado migración propiedad UrlArchivo

---
+++ APIRest/Migrations/20210515075125_CreadoUrlArchivoDocumentacion.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APIRest.Migrations
+{
+    public partial class CreadoUrlArchivoDocumentacion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UrlArchivo",
+                table: "Documentaciones",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UrlArchivo",
+                table: "Documentaciones");
+        }
+    }
+}
