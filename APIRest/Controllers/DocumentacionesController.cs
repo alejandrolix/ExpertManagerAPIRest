a2e3fe7538b46f10048ec01b64e229037d10c6b7
Alejandro Pons <alejandro.pons98@gmail.com>
2021-05-15 13:10:30 +0200

    Creado controlador DocumentacionesController

---
+++ APIRest/Controllers/DocumentacionesController.cs
@@ -0,0 +1,41 @@
+﻿using APIRest.Context;
+using APIRest.Models;
+using APIRest.ViewModels;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest.Controllers
+{    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentacionesController : ControllerBase
+    {
+        private ExpertManagerContext _contexto;
+        private IWebHostEnvironment _webHostEnvironment;
+
+        public DocumentacionesController(ExpertManagerContext contexto, IWebHostEnvironment webHostEnvironment)
+        {
+            _contexto = contexto;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        // GET: DocumentacionesController
+        [HttpGet("{id}")]
+        public async Task<List<Aseguradora>> Index(int id)
+        {
+            
+
+            //List<Aseguradora> aseguradoras = await _contexto.Aseguradoras                                                        
+            //                                                .ToListAsync();
+
+            return null;
+        }        
+    }
+}
