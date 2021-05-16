c11ca92869edfdc6ded59d19366c395779ef40f6
Alejandro Pons Antelo <alejandro.pons98@gmail.com>
2021-05-16 14:54:55 +0200

    Creado método para subir imagen

---
+++ APIRest/Controllers/ImagenesController.cs
@@ -0,0 +1,117 @@
+﻿using APIRest.Context;
+using APIRest.Models;
+using APIRest.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest.Controllers
+{    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagenesController : ControllerBase
+    {
+        private ExpertManagerContext _contexto;        
+
+        public ImagenesController(ExpertManagerContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        // GET: ImagenesController
+        [HttpGet("ObtenerPorIdSiniestro/{idSiniestro}")]
+        public async Task<List<DocumentacionVm>> ObtenerPorIdSiniestro(int idSiniestro)
+        {
+            List<Documentacion> documentaciones = await _contexto.Documentaciones
+                                                                 .Where(documentacion => documentacion.SiniestroId == idSiniestro)
+                                                                 .ToListAsync();
+
+            List<DocumentacionVm> documentacionesVm = documentaciones.Select(documentacion => new DocumentacionVm()
+            {
+                Id = documentacion.Id,
+                Descripcion = documentacion.Descripcion
+            })
+            .ToList();
+
+            return documentacionesVm;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<FileStreamResult> Obtener(int id)
+        {
+            Documentacion documentacion = await _contexto.Documentaciones
+                                                         .FirstOrDefaultAsync(documentacion => documentacion.Id == id);
+
+            string rutaPdf = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", documentacion.UrlArchivo);
+            rutaPdf = rutaPdf.Replace("\\", "/");
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(rutaPdf, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+
+            memory.Position = 0;
+            return File(memory, "application/pdf", Path.GetFileName(rutaPdf));
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> Subir([FromForm] ImagenVm imagenVm)
+        {
+            Siniestro siniestro = await _contexto.Siniestros
+                                                 .FirstOrDefaultAsync(siniestro => siniestro.Id == imagenVm.IdSiniestro);
+            
+            string rutaPdf = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/imagenes", imagenVm.Imagen.FileName);
+            rutaPdf = rutaPdf.Replace("\\", "/");
+
+            Imagen imagen = new Imagen()
+            {
+                Descripcion = imagenVm.Descripcion,                
+                Siniestro = siniestro,
+                UrlArchivo = rutaPdf
+            };
+
+            try
+            {
+                using (var stream = System.IO.File.Create(rutaPdf))
+                {
+                    await imagenVm.Imagen.CopyToAsync(stream);
+                }
+
+                _contexto.Add(imagen);
+                await _contexto.SaveChangesAsync();
+
+                return new JsonResult(true);
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(false);
+            }
+        }
+
+        [HttpDelete("{idSiniestro}")]
+        public async Task<JsonResult> Eliminar(int idSiniestro)
+        {
+            Documentacion documentacion = await _contexto.Documentaciones
+                                                         .FirstOrDefaultAsync(documentacion => documentacion.Id == idSiniestro);
+            try
+            {
+                if (System.IO.File.Exists(documentacion.UrlArchivo))                
+                    System.IO.File.Delete(documentacion.UrlArchivo);                
+                
+                _contexto.Remove(documentacion);
+
+                await _contexto.SaveChangesAsync();
+                return new JsonResult(true);
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(false);
+            }
+        }
+    }
+}
---
+++ APIRest/ViewModels/ImagenVm.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest.ViewModels
+{
+    public class ImagenVm
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public int IdSiniestro { get; set; }
+        public IFormFile Imagen { get; set; }
+    }
+}
