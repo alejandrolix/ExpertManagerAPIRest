b91543d4a4a7d61f3be47885e50f857a0d553e1c
Alejandro Pons <alejandro.pons98@gmail.com>
2021-05-17 19:50:44 +0200

    Creado método para obtener todos los usuarios

---
+++ APIRest/Controllers/UsuariosController.cs
@@ -0,0 +1,36 @@
+﻿using APIRest.Context;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIRest.ViewModels;
+using APIRest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private ExpertManagerContext _contexto;
+
+        public UsuariosController(ExpertManagerContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        [HttpGet]
+        public async Task<List<UsuarioVm>> ObtenerTodos()
+        {
+            List<Usuario> usuarios = await _contexto.Usuarios
+                                                    .Include(usuario => usuario.Permiso)
+                                                    .ToListAsync();
+
+            List<UsuarioVm> usuariosVms = usuarios.Select(usuario => new UsuarioVm(usuario)).ToList();
+
+            return usuariosVms;
+        }
+    }
+}
---
+++ APIRest/ViewModels/UsuarioVm.cs
@@ -0,0 +1,34 @@
+﻿using APIRest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest.ViewModels
+{
+    public class UsuarioVm
+    {
+        public int Id { get; set; }        
+        public string Nombre { get; set; }
+        public string EsPerito { get; set; }
+        public int IdPermiso { get; set; }
+        public string Permiso { get; set; }
+
+        public UsuarioVm(Usuario usuario)
+        {
+            string esPerito = "";
+
+            if (usuario.EsPerito.HasValue)
+                if (usuario.EsPerito.Value)
+                    esPerito = "Sí";
+                else
+                    esPerito = "No";
+
+            Id = usuario.Id;
+            Nombre = usuario.Nombre;
+            EsPerito = esPerito;
+            IdPermiso = usuario.Permiso.Id;
+            Permiso = usuario.Permiso.Nombre;
+        }
+    }
+}
