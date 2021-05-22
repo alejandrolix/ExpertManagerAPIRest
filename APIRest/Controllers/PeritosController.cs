fb7302ae47a5a05376fb97f542134342f5df6a4c
Alejandro Pons Antelo <alejandro.pons98@gmail.com>
2021-05-22 14:23:59 +0200

    Creado método para obtener importe reparación de daños por perito

--- APIRest/Controllers/PeritosController.cs
+++ APIRest/Controllers/PeritosController.cs
@@ -40,6 +40,16 @@ namespace APIRest.Controllers
             .ToList();
 
             return peritosVms;
-        }        
+        }
+
+        [HttpGet("ImporteReparacionDanios/{idPerito}")]
+        public async Task<JsonResult> ObtenerImpReparacionDaniosPorIdPerito(int idPerito)
+        {
+            Usuario perito = await _contexto.Usuarios
+                                            .Include(usuario => usuario.Permiso)
+                                            .FirstOrDefaultAsync(usuario => usuario.Permiso.Id != 1 && usuario.Id == idPerito);
+
+            return new JsonResult(perito.ImpRepacionDanios);
+        }
     }
 }
