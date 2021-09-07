d7b05a3685f90c90dd8aa4e9e594b48bcb5899b8
Alejandro Pons <alejandro.pons98@gmail.com>
2021-09-07 20:44:09 +0200

    Eliminado clase RespuestaApi en PeritosController.ObtenerImpReparacionDaniosPorIdPerito

--- APIRest/Controllers/PeritosController.cs
+++ APIRest/Controllers/PeritosController.cs
@@ -43,33 +43,22 @@ namespace APIRest.Controllers
         }
 
         [HttpGet("ImporteReparacionDanios/{idPerito}")]
-        public async Task<RespuestaApi> ObtenerImpReparacionDaniosPorIdPerito(int idPerito)
+        public async Task<ActionResult> ObtenerImpReparacionDaniosPorIdPerito(int idPerito)
         {
             Usuario perito = await _contexto.Usuarios
                                             .Include(usuario => usuario.Permiso)
                                             .FirstOrDefaultAsync(usuario => usuario.Permiso.Id != 1 && usuario.Id == idPerito);
-            int codigoRespuesta = 500;
             string mensaje = null;
-            object datos = null;
 
-            if (perito == null)
+            if (perito is null)
                 mensaje = $"No existe importe de reparación de daños del perito con id {idPerito}";
             else if (perito.ImpRepacionDanios == 0)
                 mensaje = $"El importe de reparación de daños del perito con id {idPerito} es cero";
-            else
-            {
-                codigoRespuesta = 200;
-                datos = perito.ImpRepacionDanios;
-            }
 
-            RespuestaApi respuestaApi = new RespuestaApi
-            {
-                CodigoRespuesta = codigoRespuesta,
-                Mensaje = mensaje,
-                Datos = datos
-            };
+            if (mensaje != null)            
+                return StatusCode(500, mensaje);            
 
-            return respuestaApi;
+            return Ok(perito.ImpRepacionDanios);
         }
     }
 }
