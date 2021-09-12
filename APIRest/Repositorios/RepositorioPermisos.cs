0e40818228c23ee26d956833074885c15e202843
Alejandro Pons <alejandro.pons98@gmail.com>
2021-09-12 18:39:46 +0200

    Creado clase RepositorioPermisos

---
+++ APIRest/Repositorios/RepositorioPermisos.cs
@@ -0,0 +1,20 @@
+﻿using APIRest.Context;
+using APIRest.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest.Repositorios
+{
+    public class RepositorioPermisos
+    {
+        private ExpertManagerContext _contexto;
+
+        public RepositorioPermisos(ExpertManagerContext contexto)
+        {
+            _contexto = contexto;
+        }
+    }
+}
