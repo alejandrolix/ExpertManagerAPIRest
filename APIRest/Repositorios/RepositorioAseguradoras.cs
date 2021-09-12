9466f1724337c8240cf69452d7cf89ac6d5b8350
Alejandro Pons <alejandro.pons98@gmail.com>
2021-09-12 18:06:11 +0200

    Creado clase RepositorioAseguradoras

---
+++ APIRest/Repositorios/RepositorioAseguradoras.cs
@@ -0,0 +1,20 @@
+﻿using APIRest.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using APIRest.Models;
+
+namespace APIRest.Repositorios
+{
+    public class RepositorioAseguradoras
+    {
+        private ExpertManagerContext _contexto;
+
+        public RepositorioAseguradoras(ExpertManagerContext contexto)
+        {
+            _contexto = contexto;
+        }
+    }
+}
