db0749c55111e7f22e437ab475dd568724a515d5
Alejandro Pons <alejandro.pons98@gmail.com>
2021-09-12 18:12:55 +0200

    Creada clase RepositorioUsuarios

---
+++ APIRest/Repositorios/RepositorioUsuarios.cs
@@ -0,0 +1,20 @@
+﻿using APIRest.Context;
+using APIRest.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest.Repositorios
+{
+    public class RepositorioUsuarios
+    {
+        private ExpertManagerContext _contexto;
+
+        public RepositorioUsuarios(ExpertManagerContext contexto)
+        {
+            _contexto = contexto;
+        }
+    }
+}
