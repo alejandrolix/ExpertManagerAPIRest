abf765bd96d896eb6c01e1a6e4017e3894a51668
Alejandro Pons <alejandro.pons98@gmail.com>
2021-09-12 18:19:00 +0200

    Creada clase RepositorioPeritos

---
+++ APIRest/Repositorios/RepositorioPeritos.cs
@@ -0,0 +1,20 @@
+﻿using APIRest.Context;
+using APIRest.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest.Repositorios
+{
+    public class RepositorioPeritos
+    {
+        private ExpertManagerContext _contexto;
+
+        public RepositorioPeritos(ExpertManagerContext contexto)
+        {
+            _contexto = contexto;
+        }
+    }
+}
