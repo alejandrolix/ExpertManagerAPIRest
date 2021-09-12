c4103515042e3571f5f7e35ab27e4e25df44c9cf
Alejandro Pons <alejandro.pons98@gmail.com>
2021-09-12 18:24:32 +0200

    Creada clase RepositorioDanios

---
+++ APIRest/Repositorios/RepositorioDanios.cs
@@ -0,0 +1,20 @@
+﻿using APIRest.Context;
+using APIRest.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest.Repositorios
+{
+    public class RepositorioDanios
+    {
+        private ExpertManagerContext _contexto;
+
+        public RepositorioDanios(ExpertManagerContext contexto)
+        {
+            _contexto = contexto;
+        }
+    }
+}
