2fc4c93ffdddfffb23d93a61f31cf912c80d2e6d
Alejandro Pons <alejandro.pons98@gmail.com>
2021-09-12 19:53:13 +0200

    Creada clase EstadisticaInicioVm

---
+++ APIRest/ViewModels/EstadisticaInicioVm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest.ViewModels
+{
+    public class EstadisticaInicioVm
+    {
+        public string NombreAseguradora { get; set; }
+        public int NumSiniestros { get; set; }
+    }
+}
