04148b04e5dd3cea25285779a58344ba744d2b06
Alejandro Pons <alejandro.pons98@gmail.com>
2021-09-12 17:51:43 +0200

    Creado método obtener estado por tipo

--- APIRest/Repositorios/RepositorioEstados.cs
+++ APIRest/Repositorios/RepositorioEstados.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using APIRest.Context;
+using APIRest.Models;
+using System;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +10,28 @@ namespace APIRest.Repositorios
 {
     public class RepositorioEstados
     {
+        private ExpertManagerContext _contexto;
+
+        public RepositorioEstados(ExpertManagerContext contexto)
+        {
+            _contexto = contexto;
+        }
+
         public enum Tipo
         {
             Procesando = 1,
             SinValorar = 2,
             Valorado = 3,
             Cerrado = 4
+        }
+
+        public async Task<Estado> ObtenerPorTipo(Tipo tipo)
+        {
+            int idEstado = (int)tipo;
+            Estado estado = await _contexto.Estados
+                                           .Where(estado => estado.Id == idEstado)
+                                           .FirstOrDefaultAsync();
+            return estado;
         }
     }
 }
