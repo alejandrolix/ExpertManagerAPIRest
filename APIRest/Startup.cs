8961ddf2d24a8ba0b74cc1a0e715020beccfb46e
Alejandro Pons <alejandro.pons98@gmail.com>
2021-09-12 18:13:18 +0200

    Añadido clase al scope

--- APIRest/Startup.cs
+++ APIRest/Startup.cs
@@ -47,6 +47,7 @@ namespace APIRest
             services.AddScoped<RepositorioSiniestros>();
             services.AddScoped<RepositorioEstados>();
             services.AddScoped<RepositorioAseguradoras>();
+            services.AddScoped<RepositorioUsuarios>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
