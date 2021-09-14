745b36b19c8db8dd537bc5377b3248ff9741e4cf
Alejandro Pons <alejandro.pons98@gmail.com>
2021-09-14 17:22:15 +0200

    Refactorizado obtener todos los daños

--- APIRest/Controllers/DaniosController.cs
+++ APIRest/Controllers/DaniosController.cs
@@ -1,11 +1,11 @@
 ﻿using APIRest.Context;
 using APIRest.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using APIRest.Repositorios;
 
 namespace APIRest.Controllers
 {    
@@ -14,19 +14,23 @@ namespace APIRest.Controllers
     public class DaniosController : ControllerBase
     {
         private ExpertManagerContext _contexto;
+        private RepositorioDanios _repositorioDanios;
 
-        public DaniosController(ExpertManagerContext contexto)
+        public DaniosController(ExpertManagerContext contexto, RepositorioDanios repositorioDanios)
         {
             _contexto = contexto;
+            _repositorioDanios = repositorioDanios;
         }
         
         [HttpGet]
-        public async Task<List<Danio>> Index()
+        public async Task<ActionResult> Index()
         {
-            List<Danio> danios = await _contexto.Danios                                                        
-                                                .ToListAsync();
+            List<Danio> danios = await _repositorioDanios.ObtenerTodos();
 
-            return danios;
+            if (danios is null || danios.Count == 0)
+                return StatusCode(500, "No existen daños");
+
+            return Ok(danios);
         }        
     }
 }
