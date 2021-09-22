08d29de5282aa720b0ced440b98925d94bfdfd75
Alejandro Pons <alejandro.pons98@gmail.com>
2021-09-22 18:05:51 +0200

    La clase CrearSiniestroVm hereda de SiniestroVm

--- APIRest/ViewModels/CrearSiniestroVm.cs
+++ APIRest/ViewModels/CrearSiniestroVm.cs
@@ -5,14 +5,8 @@ using System.Threading.Tasks;
 
 namespace APIRest.ViewModels
 {
-    public class CrearSiniestroVm
+    public class CrearSiniestroVm : SiniestroVm
     {
-        public int IdUsuarioAlta { get; set; }
-        public int IdAseguradora { get; set; }        
-        public string Direccion { get; set; }
-        public string Descripcion { get; set; }
-        public int IdDanio { get; set; }
-        public int IdSujetoAfectado { get; set; }
-        public int IdPerito { get; set; }        
+        public int IdUsuarioAlta { get; set; }               
     }
 }
