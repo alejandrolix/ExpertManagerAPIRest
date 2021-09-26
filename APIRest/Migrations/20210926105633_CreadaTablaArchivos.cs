ffc2a979ab08f7fe21695694b614ab72d4196124
Alejandro Pons <alejandro.pons98@gmail.com>
2021-09-26 13:00:42 +0200

    Creada migración tabla archivos

---
+++ APIRest/Migrations/20210926105633_CreadaTablaArchivos.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APIRest.Migrations
+{
+    public partial class CreadaTablaArchivos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Archivos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UrlArchivo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TipoArchivoId = table.Column<int>(type: "int", nullable: false),
+                    SiniestroId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Archivos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Archivos_Siniestros_SiniestroId",
+                        column: x => x.SiniestroId,
+                        principalTable: "Siniestros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Archivos_TiposArchivo_TipoArchivoId",
+                        column: x => x.TipoArchivoId,
+                        principalTable: "TiposArchivo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Archivos_SiniestroId",
+                table: "Archivos",
+                column: "SiniestroId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Archivos_TipoArchivoId",
+                table: "Archivos",
+                column: "TipoArchivoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Archivos");
+        }
+    }
+}
