1f3663443c968fb9459052571551f906d68c93fb
Alejandro Pons <alejandro.pons98@gmail.com>
2021-09-26 13:13:46 +0200

    Creada clase Enumeraciones

---
+++ APIRest/Enumeraciones/Enumeraciones.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest.Enumeraciones
+{
+    public class Enumeraciones
+    {
+        public enum TiposArchivo
+        {
+            Imagen = 1,
+            Documentacion = 2
+        }
+    }
+}
