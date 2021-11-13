5c0d9383d3fae7de7f9596ecdfb134dd22615597
Alejandro Pons <alejandro.pons98@gmail.com>
2021-11-13 15:51:44 +0100

    Creada excepción CodigoErrorHttpException

---
+++ APIRest/Excepciones/CodigoErrorHttpException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace APIRest.Excepciones
+{
+    public class CodigoErrorHttpException : Exception
+    {
+        public int CodigoErrorHttp { get; set; }
+
+        public CodigoErrorHttpException(string mensaje, int codigoErrorHttp) : base(mensaje)
+        {
+            CodigoErrorHttp = codigoErrorHttp;
+        }
+    }
+}
