3ffa7dc0d64569eafd976c158548f5debdef84ef
Alejandro Pons <alejandro.pons98@gmail.com>
2021-11-13 15:54:24 +0100

    Creado ViewModel CerrarSiniestroVm

---
+++ APIRest/ViewModels/CerrarSiniestroVm.cs
@@ -0,0 +1,9 @@
+﻿namespace APIRest.ViewModels
+{
+    public class CerrarSiniestroVm
+    {
+        public int IdSiniestro { get; set; }
+        public int IdPermiso { get; set; }
+        public int IdUsuario { get; set; }
+    }
+}
