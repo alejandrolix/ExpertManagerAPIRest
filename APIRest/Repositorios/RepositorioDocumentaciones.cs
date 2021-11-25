a02fe91b2ee814ac078d1b97058191e3adc429de
Alejandro Pons <alejandro.pons98@gmail.com>
2021-11-25 17:10:31 +0100

    Lanzado excepción si no existen documentaciones por id siniestro

--- APIRest/Repositorios/RepositorioDocumentaciones.cs
+++ APIRest/Repositorios/RepositorioDocumentaciones.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using APIRest.Models;
 using APIRest.Enumeraciones;
+using APIRest.Excepciones;
+using System.Net;
 
 namespace APIRest.Repositorios
 {
@@ -26,6 +28,9 @@ namespace APIRest.Repositorios
                                                            .Include(archivo => archivo.TipoArchivo)
                                                            .Where(archivo => archivo.Siniestro.Id == id && archivo.TipoArchivo.Id == idTipoArchDocumentacion)
                                                            .ToListAsync();
+            if (documentaciones is null)
+                throw new CodigoErrorHttpException($"No existen documentaciones con id de siniestro {id}", HttpStatusCode.NotFound);
+
             return documentaciones;
         }
 
