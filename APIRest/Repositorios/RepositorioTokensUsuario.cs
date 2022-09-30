e3cd236e9e0e48d257d02699fc40951188e8aa44
Alejandro Pons <alejandro.pons98@gmail.com>
2022-09-30 18:42:18 +0200

    Creado clase RepositorioTokensUsuario

---
+++ APIRest/Repositorios/RepositorioTokensUsuario.cs
@@ -0,0 +1,14 @@
+﻿using APIRest.Context;
+
+namespace APIRest.Repositorios
+{
+    public class RepositorioTokensUsuario
+    {
+        private ExpertManagerContext _contexto;
+
+        public RepositorioTokensUsuario(ExpertManagerContext contexto)
+        {
+            _contexto = contexto;
+        }
+    }
+}
