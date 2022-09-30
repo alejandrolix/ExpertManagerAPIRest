529b902a42172ea98673519ee663b6b9d54e68a1
Alejandro Pons <alejandro.pons98@gmail.com>
2022-09-30 18:21:56 +0200

    Creado modelo TokenUsuario

---
+++ APIRest/Models/TokenUsuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIRest.Models
+{
+    public class TokenUsuario
+    {
+        [Key]
+        public string Token { get; set; }
+
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+        public DateTime FechaDesde { get; set; }
+        public DateTime FechaHasta { get; set; }
+    }
+}
